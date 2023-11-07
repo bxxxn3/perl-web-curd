14073b071653da9f8a4242ffdc35765f81acdc4b
bxn3 <eviathan228@gmail.com>
2023-11-07 16:52:13 +0300

    use footable with js

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,12 @@
+async function loadJson() {
 
-fetch('http://127.0.0.1:3000/get_json?')
-  .then(response => response.json())
-  .then(json => console.log(json))
+
+  const response = await fetch('http://127.0.0.1:3000/get_json?');
+  const data = await response.json();
+  console.log(data);
+  console.log(typeof jsonData)
+  return data;
+
+}
+
+var jsonData = loadJson();
